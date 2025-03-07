46ce04ac79af69772bdc8fee1fb31636b2bf5d43
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-03-07 13:37:55 +0530

    Time: 91 ms (68.40%) | Memory: 50.9 MB (21.20%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        boolean[] isPrime = sieveOfEratosthenes(right);
+        
+        List<Integer> primes = new ArrayList<>();
+        for (int i = Math.max(2, left); i <= right; i++) {
+            if (isPrime[i]) {
+                primes.add(i);
+            }
+        }
+        
+        if (primes.size() < 2) {
+            return new int[]{-1, -1}; // Not enough primes
+        }
+        
+        int minDiff = Integer.MAX_VALUE;
+        int[] result = new int[2];
+        
+        for (int i = 1; i < primes.size(); i++) {
+            int diff = primes.get(i) - primes.get(i - 1);
+            if (diff < minDiff) {
+                minDiff = diff;
+                result[0] = primes.get(i - 1);
+                result[1] = primes.get(i);
+            }
+        }
+        
+        return result;
+    }
+    
+    public boolean[] sieveOfEratosthenes(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false; // 0 and 1 are not primes
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return isPrime;
+    }
+}

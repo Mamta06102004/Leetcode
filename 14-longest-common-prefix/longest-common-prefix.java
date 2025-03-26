b7071e1da26d6115e5c09f755f35bb72d3a71673
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-03-26 23:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (51.01%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,15 +3,23 @@ class Solution {
         if (strs == null || strs.length == 0) {
             return "";
         }
+
+        // Take the first string as a reference
         String prefix = strs[0];
+
+        // Compare the prefix with each string in the array
         for (int i = 1; i < strs.length; i++) {
+            // Reduce the prefix length until it matches the start of the current string
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
+
+                // If no common prefix, return an empty string
                 if (prefix.isEmpty()) {
                     return "";
                 }
             }
         }
+
         return prefix;
     }
 }

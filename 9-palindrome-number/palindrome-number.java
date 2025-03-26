bc91694ddd3d3bdef045f0fc8014a30c883cf2b8
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-03-26 23:22:19 +0530

    Time: 4 ms (100.00%) | Memory: 44.2 MB (35.94%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -6,14 +6,12 @@ class Solution {
         int original = x;
         int reversed = 0;
 
-        // Reverse the number
         while (x != 0) {
             int digit = x % 10;
             reversed = reversed * 10 + digit;
             x /= 10;
         }
 
-        // Check if the reversed number is the same as the original
         return original == reversed;
     }
 }

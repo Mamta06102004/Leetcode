5c3d27808c2924ef570324201cfc8ca0c893d807
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-03-28 17:56:07 +0530

    Time: 163 ms (23.66%) | Memory: 55.7 MB (100.00%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.java
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.java
@@ -3,22 +3,16 @@ class Solution {
         int m = grid.length, n = grid[0].length;
         int[] result = new int[queries.length];
         
-        // Store original indices of queries
         int[][] queryWithIndex = new int[queries.length][2];
         for (int i = 0; i < queries.length; i++) {
             queryWithIndex[i] = new int[]{queries[i], i};
         }
-        
-        // Sort queries in ascending order
         Arrays.sort(queryWithIndex, Comparator.comparingInt(a -> a[0]));
         
-        // Min-heap for BFS, stores {value, row, col}
         PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         minHeap.offer(new int[]{grid[0][0], 0, 0});
-        
         boolean[][] visited = new boolean[m][n];
         visited[0][0] = true;
-        
         int[] directions = {-1, 0, 1, 0, -1}; // For moving in 4 directions
         int count = 0, index = 0;
         
@@ -30,22 +24,18 @@ class Solution {
                 int[] cell = minHeap.poll();
                 int r = cell[1], c = cell[2];
                 count++;
-                
                 for (int d = 0; d < 4; d++) {
                     int nr = r + directions[d];
                     int nc = c + directions[d + 1];
-                    
                     if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
                         visited[nr][nc] = true;
                         minHeap.offer(new int[]{grid[nr][nc], nr, nc});
                     }
                 }
             }
-            
             result[originalIndex] = count;
             index++;
         }
-        
         return result;
     }
 }

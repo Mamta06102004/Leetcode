1adb84a4452ece93ca17c76ba847168ff5cf9bf7
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-03-30 18:50:33 +0530

    Time: 3 ms (95.84%) | Memory: 44.2 MB (75.81%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,17 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-
-        int ans = 1;
-        for(int i=1; i<nums.length; i++){
-            for(int j=i-1; j>=0; j--){
-                if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            int lo = 0, hi = ans;
+            while(lo < hi){
+                int m = lo + (hi-lo)/2;
+                if(dp[m] < nums[i]){
+                    lo = m + 1;
+                }
+                else{               //dp[m] > nums[i]
+                    hi = m;
                 }
             }
-            ans = Math.max(ans, dp[i]);
+            dp[lo] = nums[i];
+            if(lo == ans){
+                ans++;
+            }
         }
         return ans;
     }

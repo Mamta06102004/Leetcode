429901dad7e75137a16ed01c856395ec51e564c8
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-04-01 23:49:49 +0530

    Time: 19 ms (16.07%) | Memory: 125.9 MB (14.64%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n+1];
+        Arrays.fill(dp, -1);
+        return helper(questions, 0, dp);
+        
+    }
+    public long helper(int[][] que, int i, long[] dp){
+        if(i >= que.length){
+            return 0L;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int points = que[i][0];
+        int brainpower = que[i][1];
+        int nextque = i + brainpower + 1;
+
+        long skip = helper(que,i+1,dp);        
+        long solve = points + helper(que,nextque,dp); 
+
+        dp[i] = Math.max(skip, solve);
+        return Math.max(skip, solve);        
+
+    }
+}

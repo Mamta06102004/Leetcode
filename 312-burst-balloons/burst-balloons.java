14a80fd72d3750e1a7cad9f1f3c49e4647864442
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-04-09 14:18:30 +0530

    Time: 30 ms (98.86%) | Memory: 42.5 MB (90.48%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,36 +1,29 @@
 class Solution {
     public int maxCoins(int[] nums) {
-        // RESCURSIVE APPROACH
+        // ITERATIVE APPROACH
 
         int n = nums.length;
         int[] arr = new int[n+2];
+        int m = arr.length;
         arr[0] = 1;
         arr[arr.length-1] = 1;
         for(int i=0; i<n; i++){
             arr[i+1] = nums[i];
         }
-        int[][] dp = new int[n+2][n+2];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[m][m];
+
+        for(int gap=2; gap<m; gap++){
+            for(int s=0; s<=m-1-gap; s++){
+                int e = s+gap;
+                int ans = Integer.MIN_VALUE;
+                for(int i=s+1; i<e; i++){
+                    int myans = dp[s][i] + dp[i][e] + arr[s]*arr[i]*arr[e];
+                    ans = Math.max(ans, myans);
+                }
+                dp[s][e] = ans;
+            }
         }
 
-        return maxdon(arr, 0, arr.length-1, dp);
-    }
-
-    public int maxdon(int[] arr, int s, int e, int[][] dp){
-        if(s+1 >= e) return 0;
-
-        if(dp[s][e] != -1) return dp[s][e];
-
-        int ans = Integer.MIN_VALUE;
-        for(int i=s+1; i<e; i++){
-            int leftdon = maxdon(arr, s, i, dp);
-            int rightdon = maxdon(arr, i, e, dp);
-
-            int myans = leftdon + rightdon + arr[s]*arr[i]*arr[e];
-            ans = Math.max(ans, myans);
-        }
-        dp[s][e] = ans;
-        return ans;
+        return dp[0][m-1];
     }
 }

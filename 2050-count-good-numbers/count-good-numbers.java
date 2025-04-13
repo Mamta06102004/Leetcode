6424bfcc600acb5b1f44eff42e2f31b8acff0593
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-04-13 11:35:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (37.30%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+    public int countGoodNumbers(long n) {
+        long evenCount = (n + 1) / 2;
+        long oddCount = n / 2;
+
+        long goodEven = modPow(5, evenCount, MOD); 
+        long goodOdd = modPow(4, oddCount, MOD);   
+
+        return (int)((goodEven * goodOdd) % MOD);
+    }
+
+    private long modPow(long base, long exp, int mod) {
+        long result = 1;
+        base %= mod;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+
+        return result;
+    }
+}

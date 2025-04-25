5503a5c5a362f29477bf5377f3fd7ca983e1280b
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-04-25 19:23:24 +0530

    Time: 37 ms (36.70%) | Memory: 56.1 MB (93.58%) - LeetSync

---
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+        long count = 0, equals = 0;
+        Map<Integer, Long> mpp = new HashMap<>();
+        mpp.put(0, 1L);
+        for (int i : nums) {
+            if (i % modulo == k) equals++;
+            int rem = (int)(equals % modulo);
+            int needed = (rem - k + modulo) % modulo;
+            count += mpp.getOrDefault(needed, 0L);
+            mpp.put(rem, mpp.getOrDefault(rem, 0L) + 1);
+        }
+        return count;
+
+        // BRUTE FORCE
+        // long ans = 0;
+        // for(int i=0; i<nums.size(); i++){
+        //     int count = 0;
+        //     for(int j=i; j<nums.size(); j++){
+        //         if(nums.get(j) % modulo == k) count++;
+        //         if(count % modulo == k) ans++;
+        //     }
+        // }
+        // return ans;   
+    }
+
+    
+}

2b743eafc2eef399cb18e68b75f429d54875ee72
MAMTA <156337073+Mamta06102004@users.noreply.github.com>
2025-04-26 18:05:28 +0530

    Time: 37 ms (36.19%) | Memory: 55.6 MB (100.00%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
@@ -11,18 +11,33 @@ class Solution {
             mpp.put(rem, mpp.getOrDefault(rem, 0L) + 1);
         }
         return count;
-
-        // BRUTE FORCE
-        // long ans = 0;
-        // for(int i=0; i<nums.size(); i++){
-        //     int count = 0;
-        //     for(int j=i; j<nums.size(); j++){
-        //         if(nums.get(j) % modulo == k) count++;
-        //         if(count % modulo == k) ans++;
+        // int res = 0;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     int cnt = 0;
+        //     for (int j = i; j < nums.size(); j++) {
+        //         if (nums.get(j) % modulo == k) cnt++;
+        //         if (cnt % modulo == k) res++;
         //     }
         // }
-        // return ans;   
-    }
+        // return res;
+        // HashSet<Integer> set = new HashSet<>(); 
+        // for(int i=0; i<nums.size(); i++){
+        //     set.add(nums[i]);
+        // }
+        // int k = set.size();
+        // int count = 0;
 
-    
+        // for(int sp=0; sp<nums.length; sp++){
+        //     HashSet<Integer> set2 = new HashSet<>();   
+        //     for(int ep=sp; ep<nums.length; ep++){
+        //         set2.add(nums[ep]);
+        //         if(set2.size() == k){
+        //             count++;
+        //             // count += nums.length - ep;
+        //             // break;
+        //         }
+        //     }
+        // }
+        // return count;
+    }
 }
